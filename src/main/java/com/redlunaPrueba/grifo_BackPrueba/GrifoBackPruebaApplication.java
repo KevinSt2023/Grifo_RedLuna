c0d2df263977597bec97ade2519696cd5288b903
KEVIN <sophiadelacruz123123@gmail.com>
2025-04-30 00:57:18 -0500

    Creando espacio de trabajo Git-VS-Java-SpringBoot

---
+++ src/main/java/com/redlunaPrueba/grifo_BackPrueba/GrifoBackPruebaApplication.java
@@ -0,0 +1,13 @@
+package com.redlunaPrueba.grifo_BackPrueba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrifoBackPruebaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrifoBackPruebaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/redlunaPrueba/grifo_BackPrueba/GrifoBackPruebaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.redlunaPrueba.grifo_BackPrueba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrifoBackPruebaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
